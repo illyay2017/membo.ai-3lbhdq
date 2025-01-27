55df6d53fc25c67c38a2f342fa4a14d4bcbb27eb
Illya Yanchuk <illya@Illyas-MacBook-Pro-2.local>
2025-01-27 16:22:29 -0500

    making progress on webapp

--- src/web/postcss.config.js
+++ src/web/postcss.config.js
@@ -4,20 +4,11 @@
  * @version 1.0.0
  */
 
-module.exports = {
-  plugins: [
-    // Enable @import resolution and CSS modules
-    require('postcss-import')(), // v15.x
-
-    // Process Tailwind CSS directives and utilities
-    require('tailwindcss')({ // v3.x
-      config: './tailwind.config.ts'
-    }),
-
-    // Add vendor prefixes for browser compatibility
-    require('autoprefixer')({ // v10.x
-      flexbox: 'no-2009', // Modern flexbox implementation
-      grid: 'autoplace' // Enable grid autoplace features
-    })
-  ]
+export default {
+  plugins: {
+    'postcss-import': {},
+    'tailwindcss': {},
+    'autoprefixer': {},
+    'postcss-nesting': {},
+  }
 };
